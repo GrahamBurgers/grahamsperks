286d60f48649a20be26e15780c9e91e1741450bd
Graham <catface238@gmail.com>
2023-05-28 02:41:57 -0400

    foam armor tweak

--- files/scripts/foam_armor_remove.lua
+++ files/scripts/foam_armor_remove.lua
@@ -1,5 +1,7 @@
 local owner = EntityGetRootEntity(GetUpdatedEntityID())
 local comp = EntityGetFirstComponent(owner, "DamageModelComponent") or 0
 EntityInflictDamage(owner, 0.8, DAMAGE_CURSE, "$graham_name_foamarmor", DISINTEGRATED, 0, 0)
-ComponentSetValue2(comp, "hp", ComponentGetValue2(comp, "hp") - 0.8)
-ComponentSetValue2(comp, "max_hp", ComponentGetValue2(comp, "max_hp") - 0.8)
+if comp ~= 0 then
+    ComponentSetValue2(comp, "hp", ComponentGetValue2(comp, "hp") - 0.8)
+    ComponentSetValue2(comp, "max_hp", ComponentGetValue2(comp, "max_hp") - 0.8)
+end
