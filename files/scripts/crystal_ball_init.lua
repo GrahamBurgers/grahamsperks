c923413980b3d739cb22ac5b6e62df8805976b2d
Graham <catface238@gmail.com>
2023-06-17 22:13:42 -0400

    crystal ball messages

--- files/scripts/crystal_ball_init.lua
+++ files/scripts/crystal_ball_init.lua
@@ -1,7 +1,7 @@
 if GameHasFlagRun("graham_crystalball") then
     local comp = EntityGetFirstComponent(GetUpdatedEntityID(), "ItemComponent") or 0
     SetRandomSeed(comp, GameGetFrameNum())
-    local options_max = 45
+    local options_max = 40
     local option = "$graham_wisdom_" .. tostring(Random(1, options_max))
     local message = GameTextGet("$graham_wisdom_start", GameTextGetTranslatedOrNot(option))
     ComponentSetValue2(comp, "ui_description", message)
