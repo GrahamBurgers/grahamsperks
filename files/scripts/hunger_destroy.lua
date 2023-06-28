ec80e37e42df05f39b31a5f5ff7361c9fc0f4e88
Graham <catface238@gmail.com>
2023-06-28 01:41:27 -0400

    hunger conversion tweaks
    thank you nathan

--- files/scripts/hunger_destroy.lua
+++ files/scripts/hunger_destroy.lua
@@ -1,5 +1,5 @@
 function material_area_checker_success( x, y )
-    ConvertMaterialEverywhere(CellFactory_GetType("graham_graymatter_liquid"), CellFactory_GetType("graham_yum"))
+    EntityLoad("mods/grahamsperks/files/entities/hunger_convert.xml", x, y)
     EntityLoad("data/entities/particles/supernova.xml", x, y)
     GamePlaySound( "data/audio/Desktop/misc.bank", "misc/sun/supernova", x, y )
     AddFlagPersistent("graham_progress_appeased")
