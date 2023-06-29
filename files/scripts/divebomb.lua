8fa0ac28e8041eda006c5c18419be12723c79770
Graham <catface238@gmail.com>
2023-06-29 03:12:27 -0400

    dive bomb bettering
    it's sort of situational how accurate it is

--- files/scripts/divebomb.lua
+++ files/scripts/divebomb.lua
@@ -1,7 +1,7 @@
 local DISTANCE = 120
 local me = GetUpdatedEntityID()
 local x, y = EntityGetTransform(me)
-local enemies = EntityGetInRadiusWithTag(x, y, DISTANCE, "mortal") or {}
+local enemies = EntityGetInRadiusWithTag(x, y, DISTANCE, "enemy") or {}
 local comp = EntityGetFirstComponentIncludingDisabled(me, "ProjectileComponent")
 local whoshot = 0
 if comp ~= nil then
@@ -10,14 +10,13 @@ end
 for i, enemy in ipairs(enemies) do
     local x2, y2 = EntityGetTransform(enemy)
     -- remember, up is down
-    -- also try not to hit propane tanks
-    if y < y2 and math.abs(x - x2) < DISTANCE / 12 and RaytracePlatforms( x - 4, y, x2 + 4, y2) ~= true and EntityGetComponent(enemy, "PhysicsBodyComponent") == nil and whoshot ~= enemy then
+    if y < y2 and math.abs(x - x2) < DISTANCE / 12 and RaytracePlatforms( x - 4, y, x2 + 4, y2) ~= true and whoshot ~= enemy then
         dofile_once("data/scripts/lib/utilities.lua")
 
         edit_component( me, "VelocityComponent", function(comp,vars)
             local vel_x,vel_y = ComponentGetValueVector2( comp, "mVelocity")
         
-            vel_y = math.max(vel_y, 0) + math.abs(vel_x) * 1.2
+            vel_y = math.max(vel_y, 0) + math.abs(vel_x) * 1.4
             vel_x = ((x2 - x) * 8) - (DISTANCE / math.abs(y2 - y)) -- try to course-correct to the enemy
         
             ComponentSetValueVector2( comp, "mVelocity", vel_x, vel_y)
