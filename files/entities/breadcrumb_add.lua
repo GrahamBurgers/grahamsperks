b1e7585572e1ff642d1b16cad8b37139db3060ad
Graham <catface238@gmail.com>
2023-06-29 10:58:45 -0400

    breadcrumb warm-fix

--- files/entities/breadcrumb_add.lua
+++ files/entities/breadcrumb_add.lua
@@ -1,8 +1,7 @@
 local me = GetUpdatedEntityID()
-local x, y = EntityGetTransform(me)
 
 EntityAddComponent2(me, "LuaComponent", {
 	script_source_file = "mods/grahamsperks/files/entities/breadcrumb.lua",
-	remove_after_executed = true,
+	remove_after_executed = false,
 	execute_every_n_frame = 1,
 })
