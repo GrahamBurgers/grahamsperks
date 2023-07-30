6e6b6b8d431d0d6154bc3205f4b210125935155b
Graham <catface238@gmail.com>
2023-07-30 00:35:05 -0400

    chaotic teleportatium is now more chaotic

--- files/scripts/chaotic_teleport.lua
+++ files/scripts/chaotic_teleport.lua
@@ -2,11 +2,27 @@ local entity = EntityGetRootEntity(GetUpdatedEntityID())
 local x, y = EntityGetTransform(entity)
 local x2, y2 = 123, 321
 
+SetRandomSeed(x + entity, y + GameGetFrameNum())
+if Random(1, 15) < 15 then return end
+
 -- try 80 times to teleport, do nothing if it fails
 for i = 1, 80 do
     SetRandomSeed(x + i, y + GameGetFrameNum())
-    x2 = x + Random(-200, 200) * 3
-    y2 = y + Random(-100, 100)
+    x2 = x + Random(-150, 150) * 3
+    y2 = y + Random(-80, 80)
+    -- influence from cursor (but backwards)
+    local comp = EntityGetFirstComponent(entity, "Inventory2Component")
+    if comp ~= nil then
+        local item = ComponentGetValue2(comp, "mActiveItem")
+        if item ~= nil then
+            local _, _, dir = EntityGetTransform(item)
+            x2 = x2 - (math.cos(dir) * 50)
+            y2 = y2 - (math.sin(dir) * 50)
+        end
+    end
+    local power = Random(0.4, 0.8)
+    x2 = x2 * power
+    y2 = y2 * power
     if RaytracePlatforms(x2 + 20, y2 + 20, x2 - 20, y2 - 20) == false and RaytracePlatforms(x2 - 20, y2 + 20, x2 + 20, y2 - 20) == false and DoesWorldExistAt(x2-10, y2-10, x2+10, y2+10) then
         EntityLoad( "data/entities/particles/teleportation_source.xml", x, y )
         EntityLoad( "data/entities/particles/teleportation_target.xml", x2, y2 )
@@ -19,7 +35,3 @@ for i = 1, 80 do
         break
     end
 end
-
--- chaos, chaos!
-local comp = GetUpdatedComponentID()
-ComponentSetValue2(comp, "execute_every_n_frame", Random(5, 180))
