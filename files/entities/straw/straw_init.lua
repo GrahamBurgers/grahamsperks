e9cf3a83f706c846a038004f6ff0282d96c01d5b
Human <54731850+Ramiels@users.noreply.github.com>
2023-08-18 21:57:52 -0400

    added 2 straws

--- files/entities/straw/straw_init.lua
+++ files/entities/straw/straw_init.lua
@@ -1,5 +1,5 @@
 local comp = EntityGetFirstComponentIncludingDisabled(GetUpdatedEntityID(), "SpriteComponent") or 0
-local straws = 9
+local straws = 11
 SetRandomSeed(GameGetFrameNum(), comp)
 ComponentSetValue2(comp, "image_file", "mods/grahamsperks/files/entities/straw/straw_" .. Random(1, straws) .. ".png")
 EntitySetComponentIsEnabled(GetUpdatedEntityID(), comp, true)
