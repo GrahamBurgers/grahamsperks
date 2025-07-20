2fe8a3ee74d68ef538986ecd40498ead9b28b8df
Graham <catface238@gmail.com>
2025-07-20 00:51:41 -0400

    big boom

--- files/scripts/bramball_half.lua
+++ files/scripts/bramball_half.lua
@@ -1,9 +1,20 @@
 local me = GetUpdatedEntityID()
 local proj = EntityGetFirstComponent(me, "ProjectileComponent")
+
+local x, y = EntityGetTransform(me)
+local die = EntityGetInRadiusWithTag(x, y, 2, "graham_brambles_active")
+if #die > 64 then -- PANIC!
+    EntityLoad("data/entities/projectiles/deck/death_cross_big_explosion.xml", x, y)
+    for i = 1, #die do
+        EntityKill(die[i])
+        EntityKill(me)
+    end
+    return
+end
+
 -- force turn into brambles after losing some lifetime
 if proj and EntityHasTag(me, "graham_bramball") then
     if ComponentGetValue2(proj, "lifetime") <= 120 then
-        local x, y = EntityGetTransform(me)
         EntityLoad("mods/grahamsperks/files/entities/bramball_caller.xml", x, y)
     end
 end
